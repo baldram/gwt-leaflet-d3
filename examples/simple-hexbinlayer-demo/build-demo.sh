0103519096e12079ab680e8c98c1e8df1c711cc9
Marcin Szałomski <baldram@users.noreply.github.com>
2018-08-24 20:23:19 +0200

    Fix https issue for demo at GitHub pages (#6)

--- examples/simple-hexbinlayer-demo/build-demo.sh
+++ examples/simple-hexbinlayer-demo/build-demo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "-> Build demo"
-#mvn clean package
+mvn clean package
 
 echo "-> Copy built demo"
 rm -rf ../../docs/demos/*
 cp -r target/gwt-leaflet-d3-*/DemoApp ../../docs/demos/simple-hexbinlayer-demo
-#git add ../../docs/demos/
+git add ../../docs/demos/
 
 echo "Success!"
