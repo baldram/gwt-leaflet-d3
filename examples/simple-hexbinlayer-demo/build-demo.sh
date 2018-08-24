5c6ff9c06ea90a8884cd5bc5a108baa8d73c6f3b
baldram <baldram+github@gmail.com>
2018-08-24 20:06:06 +0200

    Add GitHub pages to the project

--- examples/simple-hexbinlayer-demo/build-demo.sh
+++ examples/simple-hexbinlayer-demo/build-demo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "-> Build demo"
-mvn clean package
+#mvn clean package
 
 echo "-> Copy built demo"
-rm -rf dist/*
-cp -r target/gwt-leaflet-d3-*/DemoApp dist/
-git add dist
+rm -rf ../../docs/demos/*
+cp -r target/gwt-leaflet-d3-*/DemoApp ../../docs/demos/simple-hexbinlayer-demo
+#git add ../../docs/demos/
 
 echo "Success!"
